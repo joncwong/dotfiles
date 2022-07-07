e7dfd77e20f3d4759204db1d1277b44be7759c8d
Jonathan Wong <chi.jonathanwong@gmail.com>
2022-07-07 13:55:30 -0700

    Update and rename personalize to install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+curl -L git.io/antigen > ~/.zsh/antigen.zsh
+source ~/.zsh/antigen.zsh
+source ~/.zshrc
+sudo add-apt-repository ppa:jonathonf/vim
+sudo apt update
+sudo apt install vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
